54a74ea0922e250ffdb01eb1f90ac0b07b38c099
ShivansH110105 <shivanshagr448@gmail.com>
2026-07-01 10:57:17 +0530

    Case statement in procedural blocks.

---
+++ Verilog Language/4. Procedures/6. Priority encoder.v
@@ -0,0 +1,16 @@
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input [3:0] in,
+    output reg [1:0] pos );
+    
+    always @(*) begin // Combinational always block
+        case(in)
+            4'b0001, 4'b0011, 4'b0101, 4'b0111, 4'b1001, 4'b1011, 4'b1101, 4'b1111: pos = 2'b00;
+            4'b0010, 4'b0110, 4'b1010, 4'b1110: pos = 2'b01;
+            4'b0100, 4'b1100: pos = 2'b10;
+            4'b1000: pos = 2'b11;
+            default: pos = 0; // It executes when in dosen't matches with any of them
+        endcase
+    end
+
+endmodule
